855be6396e966a0a7f32a0828f73a3a034f7ee6a
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-05 16:12:01 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -29,11 +29,9 @@ class CashRegister
   end
 
   def add_item(title, price, quantity = 1)
-
     self.total = self.total + (price * quantity)
     self.total
     @items.fill(title,quantity)
-
   end
 
   def apply_discount
